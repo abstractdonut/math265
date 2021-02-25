aaa89ebc7d0f29766aac87bb4a892e0064434b11
micah <abstractdonut@gmail.com>
2021-02-24 17:20:18 -0800

    added euler method

--- euler.py
+++ euler.py
@@ -1,6 +1,10 @@
 import numpy as np
 
 
+# yp is a function with parameters t and y, such as lambda t, y: 3t + 4y
+# p0 is the initial condition y(p0[0]) = p0[1]
+# dt is the change in t
+# tf is the final value of t in the table
 def euler(yp, p0, dt, tf):
     t0, y0 = p0
     n = (tf - t0 + dt) / dt
